bf56a8f28a38b21e1224bc48dc47e5f8bc899c7f
miroiu <miroiu.emanuel@gmail.com>
2025-11-27 18:20:40 +0200

    feat: Add folder structure

---
+++ server/chatty-api/Chatty.Application/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Chatty.Application.Chat;
+using Chatty.Application.Common;
+using Chatty.Application.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chatty.Application;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services
+            .AddCommon()
+            .AddChat()
+            .AddUsers();
+    }
+}
---
+++ server/chatty-api/Chatty.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Chatty.Infrastructure.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chatty.Infrastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services
+            .AddPersistence();
+    }
+}
